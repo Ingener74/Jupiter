d865e9fc048a22998e287e244cfdf2d405ed1dc6
Shnayder Pavel <pavel_shnayder@3divi.com>
2014-06-10 07:56:00 +0600

    added lua virtual machine

--- Asteroids/jni/Android.mk
+++ Asteroids/jni/Android.mk
@@ -2,53 +2,56 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/libpng/Android.mk
+include $(LOCAL_PATH)/lua/Android.mk
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := native-activity
 
 # LOCAL_CPPFLAGS := -DNDK_GAME_DEBUG
-#    Game/GameBuilder.cpp                \
+
+LOCAL_CFLAGS    := -DLUA_ANSI
+# LOCAL_CPPFLAGS := -DNDK_GAME_DEBUG
     
-LOCAL_SRC_FILES :=                      \
-    Engine/Sprite.cpp                   \
-    Engine/Texture.cpp                  \
-    Engine/TextureLoader.cpp            \
-    Engine/Tools.cpp                    \
-    Engine/AssetTextureLoader.cpp       \
-    Engine/Image.cpp                    \
-    Engine/GLES20Engine.cpp             \
-    Engine/Scene.cpp                    \
-    Engine/SimpleShaderLoader.cpp       \
-    Engine/DummyTextureLoader.cpp       \
-    Engine/SimpleSpriteLoader.cpp       \
-    Engine/RectSpriteLoader.cpp         \
-    Engine/Rect.cpp                     \
-    Engine/SLESSound.cpp                \
-    Engine/SLESSoundEngine.cpp          \
-    Engine/DummySound.cpp               \
-    Engine/DummySoundEngine.cpp         \
-                                        \
-    Game/AssetShaderLoader.cpp          \
-    Game/BackGround.cpp                 \
-    Game/StartButton.cpp                \
-    Game/BattleShip.cpp                 \
-    Game/FireButton.cpp                 \
-    Game/RightButton.cpp                \
-    Game/LeftButton.cpp                 \
-    Game/GasButton.cpp                  \
-    Game/Bullet.cpp                     \
-    Game/Rock.cpp                       \
-    Game/GameAgain.cpp                  \
-    Game/WinAgain.cpp                   \
-    Game/Explosion.cpp                  \
-    Game/Life.cpp                       \
-    Game/Game.cpp                       \
-                                        \
-    main.cpp                            \
+LOCAL_SRC_FILES :=                 \
+    Engine/Sprite.cpp              \
+    Engine/Texture.cpp             \
+    Engine/TextureLoader.cpp       \
+    Engine/Tools.cpp               \
+    Engine/AssetTextureLoader.cpp  \
+    Engine/Image.cpp               \
+    Engine/GLES20Engine.cpp        \
+    Engine/Scene.cpp               \
+    Engine/SimpleShaderLoader.cpp  \
+    Engine/DummyTextureLoader.cpp  \
+    Engine/SimpleSpriteLoader.cpp  \
+    Engine/RectSpriteLoader.cpp    \
+    Engine/Rect.cpp                \
+    Engine/SLESSound.cpp           \
+    Engine/SLESSoundEngine.cpp     \
+    Engine/DummySound.cpp          \
+    Engine/DummySoundEngine.cpp    \
+                                   \
+    Game/AssetShaderLoader.cpp     \
+    Game/BackGround.cpp            \
+    Game/StartButton.cpp           \
+    Game/BattleShip.cpp            \
+    Game/FireButton.cpp            \
+    Game/RightButton.cpp           \
+    Game/LeftButton.cpp            \
+    Game/GasButton.cpp             \
+    Game/Bullet.cpp                \
+    Game/Rock.cpp                  \
+    Game/GameAgain.cpp             \
+    Game/WinAgain.cpp              \
+    Game/Explosion.cpp             \
+    Game/Life.cpp                  \
+    Game/Game.cpp                  \
+                                   \
+    main.cpp                       \
 
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2 -lz -lOpenSLES
-LOCAL_STATIC_LIBRARIES := android_native_app_glue png
+LOCAL_STATIC_LIBRARIES := android_native_app_glue png lua
 
 include $(BUILD_SHARED_LIBRARY)
 
