68a1f70f76ee4eb14aca55707443d60524b2010d
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-07-31 11:25:40 +0400

    added base for game player for linux and in future for windows

--- samples/Asteroids/Asteroids.lua
+++ samples/Asteroids/Asteroids.lua
@@ -5,27 +5,30 @@ viewport = {
     height = 600;
 }
 
+background_sprite = {
+    texture = "bg.png",
+    x = 0,
+    y = 0
+}
+
 scenes = {
     {
         name = "Start",
         sprites = {
-            {
-                texture = "bg.png",
-                x = 0,
-                y = 0
-            },
-            {
-                texture = "start_button.png",
-                x = 0,
-                y = 0
-            }
+            background_sprite
         },
         game_objects = {
-            bg = {
-                name = "bg",
+            start_button = {
+                name = "start button",
+                sprites = {
+                    {
+                        texture = "start_button.png",
+                        x = 0,
+                        y = 0
+                    }
+                },
                 
                 bla = 123,
-                
                 controller = function(bg)
                     print("background controller", bg.name)
                 end,
@@ -43,8 +46,34 @@ scenes = {
     {
         name = "Main",
         sprites = {
+            background_sprite
         },
         game_objects = {
+            battleship = {
+                name = "Battle ship",
+                
+                sprites = {
+                    -- one texture two spirites, different texture coords
+                    body = {
+                        texture = "battleship.png",
+                        x = 0,
+                        y = 0,
+                    },
+                    fire = {
+                        texture = "battleship.png",
+                        x = 0,
+                        y = 0,
+                    }
+                },
+                onInput = function(battleship, x, y)
+                    if x > 100 and x < 300 then
+                        print("battleship pressed")
+                    end
+                end,
+                onUpdate = function(battleship, dt)
+                    print("update battleship = ", dt)
+                end
+            }
         }
     },
     {
@@ -63,7 +92,6 @@ scenes = {
     }
 }
 
---[[ 
 print("viewport ", viewport.x, " ", viewport.y, " ", viewport.width, " ", viewport.height, " ")
 for i, n in pairs(scenes) do
     print(i, " -> ", n.name);
@@ -72,14 +100,17 @@ for i, n in pairs(scenes) do
     end
     for s1, g in pairs(n.game_objects) do
         print("\t", s1, " -> ", g.name)
-        g:controller()
-        g:onInput(100, 123)
+        if g.controller ~= nil then
+            g:controller()
+        end
+        if g.onInput ~= nil then
+            g:onInput(100, 123)
+        end
         if g.onUpdate ~= nil then
             g:onUpdate(0.1)
         end
     end
 end
---]]
 
 
 
