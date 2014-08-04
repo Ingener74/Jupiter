23dc62d05b24b24eb19ba82e3240fd9522569e35
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-08-04 09:15:44 +0400

    added glviewer in game designer

---
+++ apps/GameDesigner/src/GLViewer.h
@@ -0,0 +1,20 @@
+/*
+ * GLViewer.h
+ *
+ *  Created on: Aug 4, 2014
+ *      Author: pavel
+ */
+
+#ifndef GLVIEWER_H_
+#define GLVIEWER_H_
+
+#include <QtWidgets/QWidget>
+
+class GLViewer: public QGLWidget
+{
+public:
+	GLViewer();
+	virtual ~GLViewer();
+};
+
+#endif /* GLVIEWER_H_ */
