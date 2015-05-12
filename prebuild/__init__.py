6c316412dfe34ca4aaf6d3ad829a29486aee4fd5
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-05-12 18:55:39 +0300

    Добавил нормальную загрузку модулей, сделал главное окно

---
+++ prebuild/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from Linux import Linux
+from Downloader import Downloader
+from Builder import Builder
+
--- prebuild_dependencies.py
+++ prebuild_dependencies.py
@@ -3,12 +3,10 @@
 
 import sys
 import subprocess
-from PySide.QtGui import QApplication, QFileDialog
+from PySide.QtGui import QApplication, QFileDialog, QMainWindow, QGroupBox, QLabel, QVBoxLayout, QHBoxLayout, QWidget
+from PySide import QtCore
 
-from prebuild.Linux import Linux
-from prebuild.Downloader import Downloader
-from prebuild.Builder import Builder
-
+from prebuild import Linux, Downloader, Builder
 
 class BoostBuilder(Builder):
     def __init__(self, downloader):
@@ -20,15 +18,45 @@ class BoostBuilder(Builder):
         print 'after download'
 
 
+class MainWindow(QWidget):
+    MIN_SIZE = 600
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        
+        test = QLabel(u'Test')
+        envLayout = QVBoxLayout()
+        envLayout.addWidget(test)
+        
+        environment = QGroupBox(u'Окружение')
+        environment.setMinimumSize(self.MIN_SIZE, self.MIN_SIZE)
+        environment.setLayout(envLayout)
+        
+        components = QGroupBox(u'Компоненты')
+        components.setMinimumSize(self.MIN_SIZE, self.MIN_SIZE)
+        
+        horLayout = QHBoxLayout()
+        horLayout.addWidget(environment)
+        horLayout.addWidget(components)
+        self.setLayout(horLayout)
+        
+        self.setWindowTitle(u'Сборка зависимостей')
+    
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key_Escape:
+            raise SystemExit
+
 def main():
     app = QApplication(sys.argv)
     
-    downloadDirectory = QFileDialog.getExistingDirectory(None, u'Укажи директорию для закачки файлов')
-    platform = Linux(downloadDirectory)
+    mainWindow = MainWindow()
+    mainWindow.show()
     
-    boostBuilder = BoostBuilder(Downloader(u'Скачай последнюю версию библиотеки Boost', \
-                                           'http://sourceforge.net/projects/boost/files/boost/', platform))
-    boostBuilder.build()
+#     downloadDirectory = QFileDialog.getExistingDirectory(None, u'Укажи директорию для закачки файлов')
+#     platform = Linux(downloadDirectory)
+#     
+#     boostBuilder = BoostBuilder(Downloader(u'Скачай последнюю версию библиотеки Boost', \
+#                                            'http://sourceforge.net/projects/boost/files/boost/', platform))
+#     boostBuilder.build()
     
     return app.exec_()
 
