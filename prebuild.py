c34b82443fd3923b5ba9974da2824d58bafef395
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-06-19 00:30:59 +0400

    разбирался с программой сборки зависимостей

--- prebuild.py
+++ prebuild.py
@@ -1,60 +1,59 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys
+import sys, os
 import subprocess
-from PySide.QtGui import QApplication, QFileDialog, QMainWindow, QGroupBox, QLabel, QVBoxLayout, QHBoxLayout, QWidget,\
-    QTextEdit, QIcon, QPixmap
-from PySide import QtCore
+from PySide.QtGui import (QApplication, QFileDialog, QWidget)
 
 from prebuild import *
+
 
 class BoostBuilder(Builder):
     def __init__(self, downloader):
         super(BoostBuilder, self).__init__(downloader)
-    
+
     def build(self):
         self.downloader.show()
-        
+
         print 'after download'
 
 
 class MainWindow(QWidget, Ui_Main):
     MIN_SIZE = 600
+
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.setupUi(self)
-        
-        
+
+        '''
+        '''
+
+        myEnv = os.environ.copy()
+        myEnv['PATH'] = 'C:\\cygwin64\\bin;' + myEnv['PATH']
+        # print myEnv
+
+        buildDirectory = '..'
+
+        out, err = subprocess.Popen(['wget.exe', 'https://box2d.googlecode.com/files/Box2D_v2.3.0.7z', '-P', buildDirectory], stdout=subprocess.PIPE, env=myEnv).communicate()
+        # output = proc.stdout.read()
+        # print output
+
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Escape:
             raise SystemExit
 
-
-def setStyle():
-    if sys.platform == 'win32':
-        QApplication.setStyle(u"windows")
-    elif sys.platform == 'linux2':
-        QApplication.setStyle(u"plastique")
-    else:
-        print u'Неизвестная система'
-
-def main():
-    setStyle()
+# noinspection PyPep8Naming
+if __name__ == '__main__':
+    QApplication.setStyle(u'plastique')
     app = QApplication(sys.argv)
-    
+
     mainWindow = MainWindow()
     mainWindow.show()
-    
-#     downloadDirectory = QFileDialog.getExistingDirectory(None, u'Укажи директорию для закачки файлов')
-#     platform = Linux(downloadDirectory)
-#     
-#     boostBuilder = BoostBuilder(Downloader(u'Скачай последнюю версию библиотеки Boost', \
-#                                            'http://sourceforge.net/projects/boost/files/boost/', platform))
-#     boostBuilder.build()
-    
-    return app.exec_()
 
+    # downloadDirectory = QFileDialog.getExistingDirectory(None, u'Укажи директорию для закачки файлов')
+    # platform = Linux(downloadDirectory)
+    # boostBuilder = BoostBuilder(Downloader(u'Скачай последнюю версию библиотеки Boost', \
+    #                                        'http://sourceforge.net/projects/boost/files/boost/', platform))
+    # boostBuilder.build()
 
-if __name__ == '__main__':
-    main()
+    sys.exit(app.exec_())
