e5b2cd35340ac8835af8bbccde120e21497dca4f
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-10-16 10:53:50 +0300

    Добавил тест загрузки модуля в встроеном модуле

---
+++ apps/Test/ViperFreaky/NextSummer/src/NextSummer.cpp
@@ -0,0 +1,36 @@
+/*
+ * NextSummer.cpp
+ *
+ *  Created on: Oct 16, 2015
+ *      Author: pavel
+ */
+
+#include <iostream>
+
+#include <python2.7/Python.h>
+
+#include "NextSummer.h"
+
+namespace nextsummer {
+
+using namespace std;
+
+NextSummer::NextSummer(std::string const& s): _s(s) {
+    cout << __PRETTY_FUNCTION__ << " " << _s << endl;
+}
+
+NextSummer::~NextSummer() {
+    cout << __PRETTY_FUNCTION__ << " " << _s << endl;
+}
+
+void NextSummer::sayHelloNextSummer() {
+    cout << "Hello, next summer" << endl;
+}
+
+void NextSummer::execPythonScript(const std::string& script) {
+    Py_Initialize();
+    PyRun_SimpleString(script.c_str());
+    Py_Finalize();
+}
+
+} /* namespace nextsummer */
