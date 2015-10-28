f1cd18ffe421de981315a625da697fa5361ab871
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-10-28 16:07:11 +0300

    снова добавил гугл мок и гугл тест всё таки надо покрыть плюсовыми тестами

---
+++ modules/Jupiter/tests/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 28, 2015
+ *      Author: pavel
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+TEST(Jupiter, NodeTest) {
+    EXPECT_EQ(1, 1);
+}
